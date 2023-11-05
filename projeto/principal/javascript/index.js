56061ac076c79667c6c41ed5fc53ad262ac6f05a
nattyymaciel <116592727+nattyymaciel@users.noreply.github.com>
2023-11-04 22:19:26 -0300

    funcionando \o/

--- projeto/principal/javascript/index.js
+++ projeto/principal/javascript/index.js
@@ -1,11 +1,11 @@
 const form = document.querySelector("#veiculo-form")
 if (form) {
     form.addEventListener("submit", function (event) {
-        submitForm(event, this);
+        submitFormCreate(event, this);
     });
 }
 
-async function submitForm(event, form) {
+async function submitFormCreate(event, form) {
     event.preventDefault();
 
     const btnSubmit = document.getElementById('salvar');
@@ -14,6 +14,23 @@ async function submitForm(event, form) {
     const jsonFormData = buildJsonFormData(form);
     const headers = buildHeaders();
     const response = await performPostHttpRequest('http://localhost:3000/veiculos', headers, jsonFormData);
+    console.log(response)
+    if (response) {
+        window.location.reload()
+    } else {
+        alert('Error')
+    }
+}
+
+async function submitFormEdit(event, form) {
+    event.preventDefault();
+
+    const btnSubmit = document.getElementById('salvar');
+    btnSubmit.disabled = true;
+    setTimeout(() => btnSubmit.disabled = false, 2000);
+    const jsonFormData = buildJsonFormData(form);
+    const headers = buildHeaders();
+    const response = await performPutHttpRequest('http://localhost:3000/veiculos/' + form.veiculoId.value, headers, jsonFormData);
     console.log(response)
     if (response) {
         window.location.reload()
@@ -46,7 +63,7 @@ function buildJsonFormData(form) {
                 jsonFormData[pair[0]] = true;
                 arCondicionadoSelecionado = true
             }
-        } else {
+        } else if (pair[0] != 'veiculoId') {
             jsonFormData[pair[0]] = pair[1];
         }
     }
@@ -167,6 +184,7 @@ function criaTr(veiculo) {
     var tdCambio = criaTd(veiculo.cambio, ".info-cambio");
     var tdArCondicionado = criaTd(veiculo.ar_condicionado, ".info-ar-condicionado");
     var tdBotaoApagar = criaTdBotaoApagar(veiculo.id)
+    var tdBotaoEditar = criaTdBotaoEditar(veiculo.id)
 
     trVeiculo.appendChild(tdLocadora);
     trVeiculo.appendChild(tdModelo);
@@ -177,6 +195,7 @@ function criaTr(veiculo) {
     trVeiculo.appendChild(tdCambio);
     trVeiculo.appendChild(tdArCondicionado);
     trVeiculo.appendChild(tdBotaoApagar);
+    trVeiculo.appendChild(tdBotaoEditar);
 
 
     return trVeiculo;
@@ -190,8 +209,6 @@ function criaTd(dado, classe) {
 
     return td;
 }
-
-
 
 function criaTdBotaoApagar(id) {
     var td = document.createElement("td");
@@ -211,8 +228,63 @@ function criaTdBotaoApagar(id) {
     return td;
 }
 
+function criaTdBotaoEditar(id) {
+    var td = document.createElement("td");
+
+    var botao = document.createElement("button")
+    botao.type = "button"
+
+    botao.textContent = "Editar"
+
+    botao.addEventListener("click", function (event) {
+        event.preventDefault();
+        editarVeiculo(id);
+    });
+
+    td.appendChild(botao);
+
+    return td;
+}
+
 async function apagarVeiculo(id) {
     console.log("Apagando o veiculo " + id);
     await performDeleteHttpRequest('http://localhost:3000/veiculos/' + id)
     window.location.reload();
+}
+
+async function editarVeiculo(id) {
+    console.log("Editando o veiculo " + id);
+
+    const resposta = await performGetHttpRequest('http://localhost:3000/veiculos/' + id, buildHeaders())
+    const veiculo = resposta[0]
+
+    form.replaceWith(form.cloneNode(true));
+
+    const formEdit = document.querySelector("#veiculo-form")
+
+    formEdit.locadora.value = veiculo.locadora;
+    formEdit.modelo.value = veiculo.modelo;
+    formEdit.marca.value = veiculo.marca;
+    formEdit.ano.value = veiculo.ano;
+    if (veiculo.motor == 1) {
+        formEdit.motor.value = '1.0';
+    } else if (veiculo.motor == 2) {
+        formEdit.motor.value = '2.0';
+    } else {
+        formEdit.motor.value = veiculo.motor;
+    }
+    formEdit.portas.value = veiculo.portas;
+    formEdit.cambio.value = veiculo.cambio;
+    if (veiculo.ar_condicionado == true) {
+        formEdit.ar_condicionado.value = 'on';
+        formEdit.ar_condicionado.checked = true;
+    } else {
+        formEdit.ar_condicionado.value = 'off';
+        formEdit.ar_condicionado.checked = false;
+    }
+    formEdit.veiculoId.value = id;
+
+    formEdit.addEventListener("submit", function (event) {
+        submitFormEdit(event, this);
+    });
 }
